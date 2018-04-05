a067efe0505362d538248e0906a61cca0933d358
zdy023 <zdy004007@126.com>
2018-04-05 23:37:01 +0800

    ver 0.3f0.1

--- src/davidchangx.charpuz/xyz/davidchangx/puzzle/CharPuzMap.java
+++
@@ -1,7 +0,0 @@
-//CharPuzMap.java
-package xyz.davidchangx.puzzle;
-import java.util.TreeMap;
-import java.util.TreeSet;
-public class CharPuzMap
-{
-}
--- src/davidchangx.charpuz/xyz/davidchangx/puzzle/Dict.java
+++
@@ -1,7 +0,0 @@
-//Dict.java
-package xyz.davidchangx.puzzle;
-public interface Dict
-{
-	public String getRandomOne();
-	public TreeSet<String> getNext(String idiom);
-}
